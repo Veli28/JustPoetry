2d857677c36145c8e7f53c8b5887308a52975c68
velly.mathebula28@gmail.com <VelaphiM@discovery.co.za>
2018-06-23 06:48:25 +0200

    -Changed the colors -updated the dependencies -did the splash screen the right-way

--- app/src/main/java/com/evolutionarysoftwares/vmathebula/justpoetry/main/SplashScreenActivity.java
+++ app/src/main/java/com/evolutionarysoftwares/vmathebula/justpoetry/main/SplashScreenActivity.java
@@ -2,6 +2,7 @@ package com.evolutionarysoftwares.vmathebula.justpoetry.main;
 
 import android.content.Intent;
 import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,12 +13,9 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splashscreen);
-
-        new Handler().postDelayed(() -> {
-            Intent justPoetryIntent = new Intent(this, MainActivity.class);
-            startActivity(justPoetryIntent);
-            finish();
-        }, 2000);
+        SystemClock.sleep(2000);
+        Intent justPoetryIntent = new Intent(this, MainActivity.class);
+        startActivity(justPoetryIntent);
+        finish();
     }
 }

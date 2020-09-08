81a3ca00b9318ba1f3d5bbf6c2940872b1d09b29
velaphimathebula <velly.mathebula28@gmail.com>
2020-09-08 10:31:31 +0200

    in app reviews

--- app/src/main/java/com/evolutionarysoftwares/vmathebula/justpoetry/poem/DisplayPoemActivity.java
+++ app/src/main/java/com/evolutionarysoftwares/vmathebula/justpoetry/poem/DisplayPoemActivity.java
@@ -4,9 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.evolutionarysoftwares.vmathebula.justpoetry.R;
+import com.google.android.play.core.review.ReviewInfo;
+import com.google.android.play.core.review.ReviewManager;
+import com.google.android.play.core.review.ReviewManagerFactory;
+import com.google.android.play.core.tasks.Task;
 
 import java.util.ArrayList;
 
@@ -19,6 +26,9 @@ public class DisplayPoemActivity extends AppCompatActivity {
     DisplayPoemAdapter adapter;
     TextView titleTextView;
     TextView poetNameTextView;
+    Button reviewApp;
+    private ReviewManager manager;
+    private ReviewInfo reviewInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +37,9 @@ public class DisplayPoemActivity extends AppCompatActivity {
 
         titleTextView = findViewById(R.id.title_textView);
         poetNameTextView = findViewById(R.id.poet_name_textView);
+        reviewApp = findViewById(R.id.review_button);
 
-
+        init();
 
         Bundle extras = getIntent().getExtras();
         poem_title = extras.getString("title");
@@ -41,5 +52,34 @@ public class DisplayPoemActivity extends AppCompatActivity {
         poemLineRecyclerView.setLayoutManager(layoutManager);
         adapter = new DisplayPoemAdapter(poemLines, this);
         poemLineRecyclerView.setAdapter(adapter);
+
+        reviewApp.setOnClickListener(view -> openReviewFlow());
+    }
+
+    private void openReviewFlow() {
+        if(reviewInfo != null) {
+            Task<Void> flow = manager.launchReviewFlow(DisplayPoemActivity.this, reviewInfo);
+            flow.addOnCompleteListener(task -> {
+
+                if(task.isSuccessful()){
+                    Toast.makeText(this, "Complete", Toast.LENGTH_SHORT).show();
+                }
+                // The flow has finished. The API does not indicate whether the user
+                // reviewed or not, or even whether the review dialog was shown. Thus, no
+                // matter the result, we continue our app flow.
+            });
+        }
+
+    }
+
+    private void init() {
+        manager = ReviewManagerFactory.create(this);
+        Task<ReviewInfo> request = manager.requestReviewFlow();
+        request.addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                // We can get the ReviewInfo object
+                 reviewInfo = task.getResult();
+            }
+        });
     }
 }
